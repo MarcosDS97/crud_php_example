9e671fd05230fb330daf5b24f8fc4c6d636e25aa
MarcosDS97 <marcosdominguezserrano@gmail.com>
2024-10-04 09:35:30 +0200

    Añado la editorial

--- bbdd/libros.sql
+++ bbdd/libros.sql
@@ -4,6 +4,7 @@ USE tienda;
 CREATE TABLE libros (
     id INT AUTO_INCREMENT PRIMARY KEY,
     autor VARCHAR(255) NOT NULL,
+    editorial VARCHAR(255) NOT NULL,
     descripcion TEXT,
     precio DECIMAL(10, 2) NOT NULL,
     stock INT NOT NULL
